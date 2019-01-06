7bb529837edd1ea13f5b52ee684bcc94cd871df3
Maximilian Sydry <maximilian.sydry@gmail.com>
2019-01-06 11:19:07 +0100

    Working on controllers

--- src/main/java/pl/coderslab/taskplanner/controller/UserController.java
+++ src/main/java/pl/coderslab/taskplanner/controller/UserController.java
@@ -1,9 +1,19 @@
 package pl.coderslab.taskplanner.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
 
-    public User saveUser
+    @Autowired
+    UserService userService;
+
+    @GetMapping("users")
+    public List<User> showUsers() {
+        return userService.findAll;
+
+    }
 }
---
+++ src/main/java/pl/coderslab/taskplanner/service/BaseService.java
@@ -0,0 +1,4 @@
+package pl.coderslab.taskplanner.service;
+
+public interface BaseService {
+}
