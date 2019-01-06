b0384e6cf4400e826b9c39c15292593211a108b0
Maximilian Sydry <maximilian.sydry@gmail.com>
2019-01-06 10:51:55 +0100

    First commit

---
+++ src/main/java/pl/coderslab/taskplanner/controller/UserController.java
@@ -0,0 +1,9 @@
+package pl.coderslab.taskplanner.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    public User saveUser
+}
